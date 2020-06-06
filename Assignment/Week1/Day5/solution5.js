caa86f52db115c7ecc795034203b22692ad61c29
veena-bakhtar-aui-fsb-1 <veena.bakhtar@gmail.com>
2020-06-06 21:31:22 +0530

    Final assignment 5 upload

--- Assignment/Week1/Day5/solution5.js
+++ Assignment/Week1/Day5/solution5.js
@@ -1,7 +1,7 @@
 // Take a number and print the first 10 multiples of that number eg. Let's take
 // number 2, first 10 multiples would be -> 2,4,6,8,10,12,14,16,18,20
 
-var number = 8;
+var number = parseInt(prompt("Enter a number: "));
 
 for (var i=1; i <= 10; i++) {
     console.log(number * i);
